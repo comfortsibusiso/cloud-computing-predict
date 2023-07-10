b176d39ed55a50c0ebafdc859149798f2fd44464
comfortsibusiso <comfortsibusiso@gmail.com>
2023-07-10 23:23:55 +0200

    Select 1

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -26,10 +26,6 @@ $(function () {
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
 <<<<<<< HEAD
                 url: "https://sbtcan13z8.execute-api.eu-west-1.amazonaws.com/Prod/Portforlio-Web-EventHandler",
-=======
-                url: "https://4ohypzp7pb.execute-api.eu-west-1.amazonaws.com/",
->>>>>>> 39c4a3c050cfa602a9cab9dfea209e3e27514480
-                // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
                 cache: false,
